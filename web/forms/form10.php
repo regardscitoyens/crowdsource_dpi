8a879a691e0a495ede8a7c36d6593c6be1a8e499
Tangui Morlier <tangui@tangui.eu.org>
2014-07-19 17:52:35 +0200

    Test with "déclaration d'un parlementaire" (after Eckert)

--- web/forms/form10.php
+++ web/forms/form10.php
@@ -1,7 +1,16 @@
-<p>Merci de saisir ci dessous les observations déclarées. Si le parlementaire n'a rien saisi ou indiqué « Néant », cliquez sur le bouton « Valider le formulaire vide ».</p>
+<p>Merci de saisir ci dessous une ligne par collaborateur déclaré par le parlementaire. Pour créer une nouvelle ligne, cliquez sur le bouton « <span class="glyphicon glyphicon-plus"></span> ». Pour supprimer une ligne, cliquez sur le bouton « <span class="glyphicon glyphicon-remove"></span> » de la ligne correspondante. Si le parlementaire n'a rien saisi ou indiqué « Néant », cliquez sur le bouton « Valider le formulaire vide ».</p>
 <table id="crowdtable" class="table">
-  <tr class="row"><th class="text-center">Observations</th></tr>
+  <tr class="row"><th class="text-center">Noms des collaborateurs parlementaires</th><th class="text-center">Identification de l'employeur ou de la struture d'emploi</th><th class="text-center">Description de l'activité professionnelle</th><th>&nbsp;</th></tr>
   <tr class="row userline">
-    <td><textarea rows="12" id="champ[0,0]" placeholder="Observations déclarées" class="form-control"></textarea></td>
+    <td><textarea id="champ[0,0]" placeholder="Nom du collab. n°1" class="form-control"></textarea></td>
+    <td><textarea id="champ[0,1]" placeholder="Employeur extra du n°1" class="form-control"></textarea></td>
+    <td><textarea id="champ[0,2]" placeholder="Activité extra du n°1" class="form-control"></textarea></td>
+    <td class="middlecenter buttons"><span class="remove"><button class="form-control btn-danger"><span class="glyphicon glyphicon-remove"></span></button></span></td>
+  </tr>
+  <tr class="row userline">
+    <td><textarea id="champ[1,0]" placeholder="Nom du collab. n°2" class="form-control"></textarea></td>
+    <td><textarea id="champ[1,1]" placeholder="Employeur extra du n°2" class="form-control"></textarea></td>
+    <td><textarea id="champ[1,2]" placeholder="Activité extra du n°2" class="form-control"></textarea></td>
+    <td class="middlecenter buttons"><span class="add"><button class="form-control btn-primary"><span class="glyphicon glyphicon-plus"></span></button></span></td>
   </tr>
 </table>
---
+++ web/forms/form12.php
@@ -0,0 +1,7 @@
+<p>Merci de saisir ci dessous les observations déclarées. Si le parlementaire n'a rien saisi ou indiqué « Néant », cliquez sur le bouton « Valider le formulaire vide ».</p>
+<table id="crowdtable" class="table">
+  <tr class="row"><th class="text-center">Observations</th></tr>
+  <tr class="row userline">
+    <td><textarea rows="12" id="champ[0,0]" placeholder="Observations déclarées" class="form-control"></textarea></td>
+  </tr>
+</table>
