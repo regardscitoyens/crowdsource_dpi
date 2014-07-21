b2ef8ba7019367de32ab2f9e1c0880718bb10804
Tangui Morlier <tangui@tangui.eu.org>
2014-07-21 23:47:47 +0200

    Work even without database

--- include/db.php
+++ include/db.php
@@ -1,8 +1,11 @@
 <?php
 
-include("config.php");
 
-try { $bdd = new PDO('mysql:host=localhost;dbname='.$DBNAME, $DBUSER, $DBPASS);
+if (file_exists('config.php') {
+    include("config.php");
+
+    try { $bdd = new PDO('mysql:host=localhost;dbname='.$DBNAME, $DBUSER, $DBPASS);
       $bdd->exec('SET NAMES utf8');
+    }
+    catch (Exception $error) { die('Erreur : '.$error->getMessage()); }
 }
-catch (Exception $error) { die('Erreur : '.$error->getMessage()); }
