07a1de3f6b58b17e21e8d05ddd7c75e20dd5111e
RouxRC <b.ooghe@gmail.com>
2014-07-22 19:13:26 +0200

    typos + form in intro text

--- include/view/intro.php
+++ include/view/intro.php
@@ -2,10 +2,9 @@
       <div class="jumbotron"><div class="row">
         <div class="col-md-12"><h1 class="text-center">Numérisons les intérêts des parlementaires</h1> </div>
         <div class="col-md-8 ">
-	<p>Depuis la promulgation de la loi sur la transprence de la vie publique, les parlemenaires doivent déclarer leurs intérêts à la haute autorité pour la transparence de la vie publique afin qu'elle puisse les rendre public et que chaque citoyen puisse juger sur ses représentants sont en situation ou non de conflit d'intérêts</p>
-        <p>Afin de permettre au plus grand nombre de prendre connaissance de leur contenu, la loi prévoit que les déclarations d'intérêts soient mises à disposition du citoyen en Open Data. C'est pour cette raison que la Haute Autorité de transparence met à disposition des données relatives aux élus qu'elle controle.</p>
-        <p>En revance, les informations contenues dans les déclarations d'intérêts ne sont pas à proprement parlé en Open Data : elles n'ont pu être publiée par la HATVP que sous la forme de PDF image qui rend l'exploitation de ces informations malaisée au vu du grand nombre de déclarations mises en ligne</p>
-        <p>Afin de rendre effectif le souhait du législateur et permettre ainsi une diffusion des informations qu'elle contiennent en Open Data, Regards Citoyens invite tout à chacun à l'aider à numériser ces infomrations dont l'importance démocratique est crutiale.</p>
+        <p>Depuis la promulgation de la <a href="">loi sur la transparence de la vie publique</a>, les parlemenaires doivent déclarer leurs intérêts à la Haute Autorité pour la Transparence de la Vie Publique en charge de les contrôler et de les rendre publics afin que chaque citoyen puisse évaluer les possibles risques de confilts d'intérêts de ses représentants.</p>
+        <p>Afin de permettre au plus grand nombre de prendre connaissance de leur contenu, la loi prévoit que les déclarations d'intérêts soient mises à disposition du citoyen en Open Data. Si la Haute Autorité pour la Transparence met à disposition un jeu de données recensant les élus et les déclarations qu'elle contrôle, les informations contenues dans les déclarations d'intérêts ne sont en revanche pas à proprement parler en Open Data : elles n'ont pu être publiées par la HATVP que scannées sous la forme de PDF images rendant l'exploitation de ces informations malaisée au vu du grand nombre d'informations mises en ligne.</p>
+        <p>Afin de réaliser effectivement le souhait du législateur et permettre la réutlisation par tous des informations qu'elle contiennent en Open Data, Regards Citoyens ouvre cette plateforme collaborative invitant tout un chacun à participer à l'effort de numérisation de ces informations d'importance démocratique cruciale.</p>
         <p class="text-right" style="margin-right: 50px"><a href="#crowdsource" class="btn btn-primary btn-lg" role="button">Participer à la numérisation &raquo;</a></p>
         </div>
         <div id="stats" class="col-md-4 well well-lg">
@@ -22,7 +21,7 @@
                 <li>Machin</li>
                 <li>Chose</li>
                 <li>Muche</li>
-               </ul>  
+               </ul>
                <span><a href="#">Consulter le top 50</a></span>
               </div>
               <div class="col-xs-12 text-center"><span class="text-muted text-center">11 000 éléments restent à numériser</span></div>
