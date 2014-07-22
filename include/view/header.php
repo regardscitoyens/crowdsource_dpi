51be7b6aa199906d6a086004f949ad84b6399308
RouxRC <b.ooghe@gmail.com>
2014-07-22 19:24:33 +0200

    more typos + links + taregt blanks

--- include/view/header.php
+++ include/view/header.php
@@ -41,7 +41,7 @@ h1 {margin-bottom: 20px;}
           <ul class="nav navbar-nav">
             <li<?php if($menu_home) echo ' class="active" '; ?>><a href="./#crowdsource">Participer</a></li>
             <li><a href="./contributeurs.php">Les contributeurs</a></li>
-            <li<?php if($menu_declaration) echo ' class="active"'; ?>><a href="http://www.hatvp.fr/consulter-les-declarations-rechercher.html">Consulter les déclarations</a></li>
+            <li<?php if($menu_declaration) echo ' class="active"'; ?>><a target="_blank" href="http://www.hatvp.fr/consulter-les-declarations-rechercher.html">Consulter les déclarations</a></li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
             <li><a href="#signin" data-toggle="modal" data-target="#signin"><span class="glyphicon glyphicon-user"></span> S'enregistrer</a></li>
