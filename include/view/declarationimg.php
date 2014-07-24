72dabbd8abf6cf10806f942a3f72e8a0fef23daf
Tangui Morlier <tangui@tangui.eu.org>
2014-07-24 02:30:17 +0200

    As the first page is not set, we have only 12 parts

--- include/view/declarationimg.php
+++ include/view/declarationimg.php
@@ -3,7 +3,7 @@
               <a title="<?php echo $nom; ?>" href="<?php echo preg_replace('#(depute|senateur)/photo/(.*)/\d+#', '$2', $avatar);?>" target="_blank"><img class="img-circle pull-left" src="<?php echo $avatar; ?>"/></a>
               <div class="media-body">
                <h3 class="page-header text-muted">Déclaration de <?php echo $nom; ?> <small><a class="text-muted" href="<?php echo $source; ?>"><span class="glyphicon glyphicon-link"></span></a></small></h3>
-              <h4><small>Partie <?php echo $partie; ?> / 13</small></h4><h4><?php echo ucfirst($section); ?></h4>
+              <h4><small>Partie <?php echo $partie; ?> / 12</small></h4><h4><?php echo ucfirst($section); ?></h4>
               </div>
              </div>
              <div class="declaration"><img width="100%" src="<?php echo $img; ?>" class="zoom"/></div>
