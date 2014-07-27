a752854c920c9a3cd3cb51b5f7cc9273d137d34b
RouxRC <b.ooghe@gmail.com>
2014-07-27 19:45:14 +0200

    redirect to home when wrong args in declaration.php

--- include/action/declaration.php
+++ include/action/declaration.php
@@ -3,6 +3,13 @@
 include(__DIR__.'/../model/document.php');
 
 $doc = get_document_from_name_and_formid($_GET['nom'], $_GET['partie']);
+if (!$doc) {
+  $cururl = "http://" . $_SERVER['HTTP_HOST']  . $_SERVER['REQUEST_URI'];
+  $baseurl = preg_replace("#/[^/]*$#", "/", $cururl);
+echo $baseurl;
+  header('Location: '.$baseurl, true, 301);
+  die();
+}
 $type = $doc['type'];
 $nom = $doc['nom'];
 $avatar = $doc['avatar'];
