83406b45306228ff6e7e07b6b46b073ba1126617
RouxRC <b.ooghe@gmail.com>
2014-07-27 15:35:24 +0200

    keep lastuserid and remove old ones

--- bin/assemble_users_by_twitter.php
+++ bin/assemble_users_by_twitter.php
@@ -2,7 +2,7 @@
 
 include(__DIR__.'/../include/model/user.php');
 
-$req = $bdd->prepare('SELECT id, nickname, twitter FROM users WHERE twitter <> "" ORDER BY nickname, twitter');
+$req = $bdd->prepare('SELECT id, nickname, twitter FROM users WHERE twitter <> "" ORDER BY nickname, twitter, id');
 $req->execute();
 $lastnick = '';
 $lasttwit = '';
@@ -17,6 +17,8 @@ while($doc = $req->fetch()) {
     $contribs = $req2->fetch();
     $req3 = $bdd->prepare("UPDATE tasks SET userid = :id WHERE userid = :lastid");
     $req3->execute(array('id' => $usid, 'lastid' => $lastusid));
+    $req4 = $bdd->prepare("DELETE FROM users WHERE id = :lastid");
+    $req4->execute(array('lastid' => $lastusid));
     echo $contribs['total']." contributions reattached to $nick $twit\n";
   }
 
